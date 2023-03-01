1945bfe01450c406b95f23dc64a60c8c527f3388
Ussama Irfan <ussama@codesorbit.com>
2023-03-01 10:00:26 +0500

    Initial Commit

---
+++ WatchLessons/WatchLessonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLessonsApp.swift
+//  WatchLessons
+//
+//  Created by Ussama Irfan on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct WatchLessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

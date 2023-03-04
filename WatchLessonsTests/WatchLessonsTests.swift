60b4ec577e17e41d0ef04680b37bbad0c59319f8
Ussama Irfan <ussama@codesorbit.com>
2023-03-05 00:28:25 +0500

    unit test for api call

--- WatchLessonsTests/WatchLessonsTests.swift
+++ WatchLessonsTests/WatchLessonsTests.swift
@@ -5,32 +5,32 @@
 //  Created by Ussama Irfan on 01/03/2023.
 //
 
+import Combine
 import XCTest
 @testable import WatchLessons
 
 final class WatchLessonsTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func testAPIWorking() {
+        let expectation = self.expectation(description: "expectation fulfiled")
+        
+        var cancellableSet: Set<AnyCancellable> = []
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+        NetworkManager.shared.fetchData().sink { completion in
+            
+            switch completion {
+                
+            case .finished:
+                expectation.fulfill()
+                
+            case .failure(_):
+                XCTFail("Fail")
+            }
+            
+        } receiveValue: { _ in
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        }.store(in: &cancellableSet)
+        
+        self.waitForExpectations(timeout: 5)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
